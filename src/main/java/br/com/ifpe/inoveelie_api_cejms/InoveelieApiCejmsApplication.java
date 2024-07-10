f10b9d2ddfca69a975f0cf79ebc10f165bcbc01e
aluno <aluno@ifpe>
2024-07-10 17:01:18 -0300

    Arquivo base inoveelie - Primeira versão

---
+++ src/main/java/br/com/ifpe/inoveelie_api_cejms/InoveelieApiCejmsApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.inoveelie_api_cejms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoveelieApiCejmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InoveelieApiCejmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpe/inoveelie_api_cejms/InoveelieApiCejmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.inoveelie_api_cejms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoveelieApiCejmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
